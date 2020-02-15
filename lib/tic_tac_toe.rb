a2fbaeabdd871d922d201f607656c065a70bf6b2
sasha walker <sashawalker31@gmail.com>
2020-02-15 02:52:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,5 +24,9 @@ class TicTacToe
   end
   
   def valid_move
-    
+    def position_taken?(board,position)
+  return false if [" ", "", nil].include?(board[position])
+  return true if ["X", "O"].include?(board[position])
+  raise "#{board[position]} is not a valid move"
+end
 end 
