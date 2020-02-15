8c0cc3f6eac64525c94188d36157e010cea07b6e
sasha walker <sashawalker31@gmail.com>
2020-02-15 02:39:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ class TicTacToe
   end
   
   def WIN_COMBINATIONS 
-    a = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], []]
+    a = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], []]
       include_array([0,1,2])
         expect(TicTacToe::WIN_COMBINATIONS).to include_array([3,4,5])
         expect(TicTacToe::WIN_COMBINATIONS).to include_array([6,7,8])
